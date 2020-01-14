e96834961e24949c4898320abbe1c62002f37d1a
Fabio Spampinato <spampinabio@gmail.com>
2020-01-14 14:49:41 +0000

    Doubled default timeout time

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ function moveUnusedPath ( filePath: string, options: Options ): Promise<Result> 
       }
 
       const loop = tryloop.exponential ({
-        timeout: 1500,
+        timeout: 3000,
         tries: 20,
         factor: 2,
         minInterval: 1,
