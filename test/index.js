db30aafb261e87db2d7c8bb0c209bf3ea149702e
Fabio Spampinato <spampinabio@gmail.com>
2023-02-12 23:10:58 +0000

    Updated dependencies

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@
 import {describe} from 'fava';
 import fs from 'node:fs';
 import path from 'node:path';
+import process from 'node:process';
 import moveUnusedPath from '../dist/index.js';
 
 /* MAIN */
